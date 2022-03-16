bd907b700ce1be59b9adb1071c169c5ae0e175e2
Rubdh <benrhu@gmail.com>
2022-03-16 14:55:52 +0100

    // ApplicationContext added

--- src/main/java/com/example/SpringForumSC/SpringForumScApplication.java
+++ src/main/java/com/example/SpringForumSC/SpringForumScApplication.java
@@ -1,16 +1,20 @@
 package com.example.SpringForumSC;
 
+import com.example.SpringForumSC.Entities.User;
 import com.example.SpringForumSC.Processes.SparkpostConfig;
 
+import com.example.SpringForumSC.Repositories.UserRepository;
 import com.sparkpost.model.responses.Response;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringForumScApplication {
 
 	public static void main(String[] args){
-		SpringApplication.run(SpringForumScApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringForumScApplication.class, args);
+		UserRepository userRepository = context.getBean(UserRepository.class);
 	}
 
 }
